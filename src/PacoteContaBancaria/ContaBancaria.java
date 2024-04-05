3bdf13c2ee3b3cce9298eaa6a7f463d764260212
Mateus Fernandes <mateusfdesousa12@gmail.com>
2024-04-04 23:33:51 -0300

    Desafio de atividades do curso da formação java na Alura-

---
+++ src/PacoteContaBancaria/ContaBancaria.java
@@ -0,0 +1,19 @@
+package PacoteContaBancaria;
+
+public class ContaBancaria {
+    private String nome_Titular;
+   private int numeroConta;
+   private Double saldo;
+    public void setNome_Titular(String nome_Titular) {
+        this.nome_Titular = nome_Titular;
+    }
+
+    public void numeroDe_Conta(int Numero){
+       this.numeroConta = numeroConta;
+   }
+
+    public void getSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+}
